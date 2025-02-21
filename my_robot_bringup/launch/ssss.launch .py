ae3cedc2d944f25a3ca7884a4bd0edf2c0548a26
HassanRamadan02 <hassan.ramadan1409@gmail.com>
2025-02-21 07:05:44 +0200

    Add files via upload

--- my_robot_bringup/launch/ssss.launch .py
+++ my_robot_bringup/launch/ssss.launch .py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+
+
+# import launch_ros
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
+
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.substitutions import Command
 import os
@@ -13,11 +17,18 @@ from ament_index_python.packages import get_package_share_path
 from launch_ros.actions import Node
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
+
+
+# TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+
+
 def generate_launch_description():
     
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+   
 
-    # include xml launch file (my_robot_gazebo.launch.xml)
+    
+    # include xml launch file
     #######################################################################
     launch_include = IncludeLaunchDescription(
         XMLLaunchDescriptionSource(
@@ -27,7 +38,64 @@ def generate_launch_description():
         )
     )
     ########################################################################
+    # urdf_path = os.path.join(get_package_share_path('my_robot_description'),
+    #                          'urdf', 'my_robot.urdf.xacro')
     
+    # robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+
+    # robot_state_publisher_node = Node(
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     parameters=[{'robot_description': robot_description}]
+    # )
+
+    # world = os.path.join(
+    #     get_package_share_directory('my_robot_bringup'),
+    #     'worlds',
+    #     'class_room.world'
+    # )
+
+    # gzserver_cmd = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py')
+    #     ),
+    #     launch_arguments={'world': world}.items()
+    # )
+
+    # gzclient_cmd = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+            
+    #     ),
+    #     launch_arguments={"-topic robot_description -entity my_robot" }
+    # )
+
+   
+    # gazebo_ros = Node(
+    #     package="robot_state_publisher",
+    #     executable="spawn_entity.py",
+    #     arguments=['-topic robot_description -entity my_robot']
+    # )
+
+    # urdf_path = os.path.join(get_package_share_path('my_robot_description'),
+    #                          'urdf', 'my_robot.urdf.xacro')
+    
+    # robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+
+    # robot_state_publisher_node = Node(
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     parameters=[{'robot_description': robot_description}]
+    # )
+    # joint_state_publisher_gui_node = Node(
+    #     package="joint_state_publisher_gui",
+    #     executable="joint_state_publisher_gui"
+    # )
+
+    # included_launch = launch_ros.actions.IncludeLaunchDescription(package='my_robot_bringup', launch='my_robot_gazebo.launch.xml')  
+    # regular_node = launch_ros.actions.Node( package='bar', node_executable='my_node', output='screen')
+
+
     map_dir = LaunchConfiguration(
         'map',
         default=os.path.join(
@@ -52,6 +120,13 @@ def generate_launch_description():
 
     return LaunchDescription([
         
+        # joint_state_publisher_gui_node,
+        # included_launch,
+        # regular_node,
+        # gzserver_cmd,
+        # gzclient_cmd,
+        # robot_state_publisher_node,
+        
         DeclareLaunchArgument(
             'map',
             default_value=map_dir,
@@ -74,6 +149,8 @@ def generate_launch_description():
                 'use_sim_time': use_sim_time,
                 'params_file': param_dir}.items(),
         ),
+
+
         Node(
             package='rviz2',
             executable='rviz2',
@@ -81,6 +158,11 @@ def generate_launch_description():
             arguments=['-d', rviz_config_dir],
             parameters=[{'use_sim_time': use_sim_time}],
             output='screen'),
-        
         launch_include
+        # Node(
+        #     package='gazebo_ros',
+        #     executable='spawn_entity.py',
+            
+        #     arguments=['-topic robot_description -entity my_robot'])
+            
     ])
